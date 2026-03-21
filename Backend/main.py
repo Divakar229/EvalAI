fb46a319c2d7e16b39ae75a2ea9a66f848e417cd
Divakar229 <divakar.reddy1809@gmail.com>
2026-03-21 23:21:52 +0530

    basic setup of backend

---
+++ Backend/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app=FastAPI(
+    title="Eval-AI"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True 
+)
+
+@app.get('/')
+def root():
+    return {"Eval-AI running"}
